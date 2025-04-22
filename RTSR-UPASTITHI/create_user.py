2315eb416b2a30df36f768705a99cd8204e58247
S. Keerti Madhavi <keertimadhavis@gmail.com>
2025-04-22 23:22:32 +0530

    Update create_user.py final

--- RTSR-UPASTITHI/create_user.py
+++ RTSR-UPASTITHI/create_user.py
@@ -1,22 +1,21 @@
 import sqlite3
 
-# === CONFIGURE ADMIN ACCOUNT HERE ===
-username = 'admin'
-password = 'admin123'
-role = 'admin'
-# ====================================
-username= 'emp1'
-password= 'emp123'
-role= 'employee'
-# Connect to the SQLite database
 conn = sqlite3.connect('attendance.db')
 c = conn.cursor()
+
 try:
     c.execute("ALTER TABLE students ADD COLUMN user_id INTEGER")
     print("✅ 'user_id' column added to 'students' table.")
 except sqlite3.OperationalError as e:
     print("⚠️ Error:", e)
 
-# Insert admin user into users table
-c.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", (username, password, role))
+# === Add Admin Account ===
+admin = ('admin', 'admin123', 'admin')
+c.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", admin)
+
+# === Add Employee Account ===
+employee = ('emp1', 'emp123', 'employee')
+c.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)", employee)
+
 conn.commit()
+print("✅ Users added successfully.")
